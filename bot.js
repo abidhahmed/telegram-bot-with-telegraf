4f81b5e089e9bb98564626c60d00806f808014e5
Abidh Ahmed <7570415+abidhahmed@users.noreply.github.com>
2017-12-07 16:48:56 +0530

    Refactor and replace node-fetch with axios instead with better error handling

--- bot.js
+++ bot.js
@@ -1,49 +1,50 @@
 'use strict'
 
 const Telegraf = require('telegraf')
-const url = require('is-url')
 const config = require('./config/config')
 const comic = require('./lib/comic')
 
 const bot = new Telegraf(config.telegramToken)
 
 const errMsg = 'Unable to fullfil this request, try again later.'
-const errConsole = 'Wrong link'
 
-bot.command('cyanide', async (ctx) => {
-  const c = await comic.cyanide()
-  if (url(c)) {
-  	ctx.replyWithPhoto({
-  	  url: c
-  	})
-  } else {
-  	console.error(errConsole)
-  	ctx.reply(errMsg)
-  }
+bot.command('cyanide', (ctx) => {
+  comic.cyanide()
+    .then(cyanideLink => {
+      ctx.replyWithPhoto({
+        url: cyanideLink
+      })
+    })
+    .catch(err => {
+      ctx.reply(errMsg)
+      console.error(err)
+    })
 })
 
-bot.command('xkcd', async (ctx) => {
-  const c = await comic.xkcd()
-  if (url(c)) {
-  	ctx.replyWithPhoto({
-  	  url: c
-  	})
-  } else {
-  	console.error(errConsole)
-  	ctx.reply(errMsg)
-  }
+bot.command('xkcd', (ctx) => {
+  comic.xkcd()
+    .then(xkcdLink => {
+      ctx.replyWithPhoto({
+        url: xkcdLink
+      })
+    })
+    .catch(err => {
+      ctx.reply(errMsg)
+      console.error(err)
+    })
 })
 
-bot.command('dinocomics', async (ctx) => {
-  const c = await comic.dinocomics()
-  if (url(c)) {
-  	ctx.replyWithPhoto({
-  	  url: c
-  	})
-  } else {
-  	console.error(errConsole)
-  	ctx.reply(errMsg)
-  }
+bot.command('dinocomics', (ctx) => {
+  comic.dinocomics()
+    .then(dinocomicsLink => {
+      ctx.replyWithPhoto({
+        url: dinocomicsLink
+      })
+    })
+    .catch(err => {
+      ctx.reply(errMsg)
+      console.error(err)
+    })
 })
 
 bot.startPolling()
